c31d20676fc84ac644264a80150ee41786cd06a2
Brayan Muñoz <bm959493@gmail.com>
2026-06-17 09:47:57 -0600

    se agrego los comandos y se modifico el formulario de inicio y se le agrego el codigo

--- Sistema Venta/Sistema Venta/Form1.cs
+++ Sistema Venta/Sistema Venta/Form1.cs
@@ -16,5 +16,42 @@ namespace Sistema_Venta
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private Form formularioActivo = null;
+
+        private void AbrirFormulario(Form form)
+        {
+            if (formularioActivo != null)
+                formularioActivo.Close();
+
+
+            formularioActivo = form;
+            form.TopLevel = false;
+
+
+            PanelContenedor.Controls.Clear();
+            PanelContenedor.Controls.Add(form);
+
+            form.Show();
+        }
+
+        private void cLIENTESToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AbrirFormulario(new FrmClientes());        }
+
+        private void pRODUCTOSToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AbrirFormulario(new FrmProductos());
+        }
+
+        private void eMPLEADOSToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AbrirFormulario(new FrmEmpleados());
+        }
     }
 }
