d21c33dc663ca25cf7625a1fb07f8773053e9c51
Brayan Muñoz <bm959493@gmail.com>
2026-06-22 09:44:56 -0600

    se agrego los botones y modificaciones al formulario productos

--- Sistema Venta/Sistema Venta/FrmProductos.cs
+++ Sistema Venta/Sistema Venta/FrmProductos.cs
@@ -16,5 +16,10 @@ namespace Sistema_Venta
         {
             InitializeComponent();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
